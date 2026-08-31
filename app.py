7133263df3ab884ca78ea5478046d47b96470505
vk980159-dot <levk980159@gmail.com>
2026-09-01 02:02:26 +0530

    Fix portfolio CSS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,8 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
